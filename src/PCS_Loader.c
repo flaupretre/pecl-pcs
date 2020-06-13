25d758a3538e54b45cf5922adc0d5561d68769d4
Francois Laupretre <francois@tekwire.net>
2020-06-13 20:56:35 +0000

    file_handle.free_filename must still be set

--- src/PCS_Loader.c
+++ src/PCS_Loader.c
@@ -247,11 +247,11 @@ static int PCS_Loader_loadNode(PCS_Node *node, int throw TSRMLS_DC)
 	file_handle.type = ZEND_HANDLE_FILENAME;
 #if ZEND_EXTENSION_API_NO <= 320180731
 	file_handle.handle.fd = 0;
-	file_handle.free_filename = 0;
 #endif
 	file_handle.handle.fp = NULL;
 	file_handle.filename = ZSTR_VAL(node->uri);
 	file_handle.opened_path = NULL;
+	file_handle.free_filename = 0;
 
 	{
 #ifdef PHP_7
